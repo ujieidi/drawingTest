a108b8b1164384859b86d5779f5768cf86b465cc
ujieidi <ujieidi@user.noreply.github.com>
2026-08-09 23:34:01 +0900

    update

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
 let drawing = false
 let erasing = false
 
+let size = 3
 let oldX
 let oldY
 
+const cursorOutline = document.getElementById("cursorOutline")
 const button = document.getElementById("button")
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
-ctx.lineCap = "round"
 
-function toggle(){erasing = !erasing}
+function toggle(){
+  erasing = !erasing
+  
+  if (erasing === true) {
+  ctx.globalCompositeOperation = "destination-out"
+  ctx.lineWidth = 20
+  size = 20
+  } else {
+  ctx.globalCompositeOperation = "source-over"
+  ctx.lineWidth = 3
+  size = 3
+  }
+  cursorOutline.style.width = size + "px"
+  cursorOutline.style.height = size + "px"
+  console.log(erasing)
+}
+
+function outlinePosition(x, y){
+  cursorOutline.style.left = x - size/2 + "px"
+  cursorOutline.style.top = y - size/2 + "px"
+}
 
 button.addEventListener("click", toggle)
 
@@ -17,12 +38,8 @@ document.addEventListener("keydown", (e) => {
   if (e.key === "1"){toggle()}
 })
 
-
-
 canvas.addEventListener("mousedown", (e) => {
   drawing = true
-  
-  const rect = canvas.getBoundingClientRect()
   
   const x = e.offsetX
   const y = e.offsetY
@@ -35,13 +52,7 @@ canvas.addEventListener("mousemove", (e) => {
   const x = e.offsetX
   const y = e.offsetY
   
-  if (erasing === true) {
-  ctx.globalCompositeOperation = "destination-out"
-  ctx.lineWidth = 20
-  } else {
-  ctx.globalCompositeOperation = "source-over"
-  ctx.lineWidth = 2
-  }
+  outlinePosition(e.clientX, e.clientY)
   
   if (drawing === true){
     ctx.beginPath()
@@ -76,13 +87,7 @@ canvas.addEventListener("touchmove", (e) => {
   const x = e.touches[0].clientX - rect.left
   const y = e.touches[0].clientY - rect.top
   
-  if (erasing === true) {
-  ctx.globalCompositeOperation = "destination-out"
-  ctx.lineWidth = 20
-  } else {
-  ctx.globalCompositeOperation = "source-over"
-  ctx.lineWidth = 2
-  }
+  outlinePosition(e.touches[0].clientX, e.touches[0].clientY)
   
   if (drawing === true){
     ctx.beginPath()
@@ -98,3 +103,12 @@ canvas.addEventListener("touchmove", (e) => {
 document.addEventListener("touchend", () => {
   drawing = false
 })
+
+function update(){
+  ctx.lineCap = "round"
+  ctx.lineWidth = 3
+  cursorOutline.style.width = 3 + "px"
+  cursorOutline.style.height = 3 + "px"
+}
+
+update()
